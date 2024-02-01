6ed123ce015255ed663296d804455040232faae5
jay-270 <jayp52887@gmail.com>
2024-02-01 12:03:59 +0530

    Ui Changes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import AddTodo from "./containers/AddTodo";
 import VisibleTodoList from "./containers/VisibleTodoList";
 
 const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>  
+  <>
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Todo list</h1>
+        <p>You can enter a task and complete it here</p>
+      </div>
+    </div>
+    <div className="card">
+      <div className="container">
+        <div className="row">
+          <AddTodo />
+        </div>
+        <div className="row">
+          <VisibleTodoList />
+        </div>
+        <div className="row d-flex">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default App;
